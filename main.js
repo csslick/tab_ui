4b81bb4313cf295524e7a3d2fc4237333dc32834
csslick <digitone@naver.com>
2021-07-19 15:56:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 window.onload = function(){
 
-  const tabs = document.querySelectorAll('#tabs ul li');
+  var tabs = document.querySelectorAll('#tabs ul li');
 
   for(let i = 0; i < tabs.length;i++) {
     tabs[i].addEventListener('click', function(e){
       // tab 콘텐츠 초기화
       for(let a = 0; a < tabs.length; a++){
-        const tab = document.querySelectorAll('.tab')[a]
+        var tab = document.querySelectorAll('.tab')[a]
         tab.style.display = 'none'
+        // 버튼 스타일 초기화
+        tabs[a].classList.remove('active');
       }
+
       // 현재 텝 표시
-      const tab = document.querySelectorAll('.tab')[i]
+      var tab = document.querySelectorAll('.tab')[i]
       tab.style.display = 'block'
+      tabs[i].classList.add('active'); // 현재버튼 스타일
     })
   }
 
